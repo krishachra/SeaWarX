00bd960de50b9e618ec2bbb1cecfc4348ab6b541
shivamchoubey2004 <shivamchoubey2004@gmail.com>
2026-05-06 22:45:47 +0530

    Footer Enhancement

--- SEAWARX/src/components/Footer.jsx
+++ SEAWARX/src/components/Footer.jsx
@@ -1,11 +1,34 @@
 import "../styles/footer.css"
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <p>Made by Narendar Kumar , Krish and Shivam</p>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
 
-export default Footer;
+      <div className="footercnt">
+
+        <div className="footerLft">
+          <h2>SeaWarX ⚓</h2>
+
+          <p>
+            Command your fleet, attack enemy waters,
+            and dominate the sea in the ultimate naval
+            battle experience.
+          </p>
+        </div>
+
+        <div className="footerRght">
+          <a href="#">Privacy</a>
+          <a href="#">Terms</a>
+          <a href="#">Support</a>
+        </div>
+
+      </div>
+
+      <p className="team-name">
+        Made by Shivam, Narendar & Krish...
+      </p>
+
+    </footer>
+  )
+}
+export default Footer
